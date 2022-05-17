68e3e92dbd7bc4e3931d221b438aebbfd020d880
mrandiwaliyudin <muhammad.randiw20@gmail.com>
2022-05-17 15:12:13 +0700

    mengganti ukuran button

--- pages/Button.js
+++ pages/Button.js
@@ -4,10 +4,10 @@ const Button = ({val, clickAction, grow = false, operator = false}) => {
   return (
     <button 
     onClick={() => clickAction(val)}
-    className={`text-white text-2xl h-1/5 cursor-pointer  rounded-lg border border-gray-900 hover:bg-gray-700  
-    ${grow ? `flex-[2_0_46%]` : `flex-[2_0_21%] `}
+    className={`text-white text-2xl h-1/5 cursor-pointer bg-slate-800 rounded-lg border border-gray-900 hover:bg-gray-700  
+    ${grow ? `flex-row basis-1/2` : `flex-row basis-16 `}
     ${grow && val === 0 ? `flex-[3_0_63%]` : `flex-[1_0_21%] `}
-    ${operator ? `bg-orange-400` : ` bg-blue-900 `}
+    
     
     
     `} 
